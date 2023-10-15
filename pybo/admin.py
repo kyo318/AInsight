4a7213f0232ad606d9bf7e7eaf9742765cbf6e88
kyo318 <choiykyo@gmail.com>
2023-10-15 23:28:43 +0900

    pybo first commit

---
+++ pybo/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from .models import Question 
+# Register your models here.
+class QuestionAdmin(admin.ModelAdmin):
+    search_field = ['subject']
+
+admin.site.register(Question)
